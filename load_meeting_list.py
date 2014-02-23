f3ea6d4347ab27c9fbfce3a6201bb39d0d0e1f0f
Ian Riley <ian.riley.915@gmail.com>
2014-02-23 14:49:54 -0600

    can load meetings into database

---
+++ load_meeting_list.py
@@ -0,0 +1,35 @@
+###
+# Loads the list of meetings from the website into the database.
+# Service Hackathon - 2/23/14 Ian Riley
+###
+
+# configure the django settings module in order to import the necessary models
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "punctuil_django.settings")
+
+from agendas.models import Meeting
+from datetime import datetime
+from re import split
+from tulsagovscraper import get_meeting_list
+
+def main():
+    # retrieve the list of meetings from the website
+    meeting_list = get_meeting_list()
+
+    for meeting in meeting_list:
+        # split the meeting information into date/time and name
+        meeting_info = split(r' - ', meeting['text'])
+        # split the date/time into seperate integers
+        dt = split(r'[\W]', meeting_info[0])
+
+        # define a date time object for the meeting
+        date_time = datetime(int(dt[2]), int(dt[0]), int(dt[1]), int(dt[3]), int(dt[4]))
+        # define a meeting object
+        meeting = Meeting(name=meeting_info[1], date=date_time, agenda_id=meeting['href'])
+
+        # print results REPLACE WITH SAVE
+        print(meeting.name)
+        print(meeting.date)
+        print(meeting.agenda_id)
+
+main()
