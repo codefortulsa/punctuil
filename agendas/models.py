af7d719e4a8ce32945038f69820472ad38da42f7
groovecoder <luke.crouch@gmail.com>
2014-02-22 18:50:45 -0600

    add agendas app with south

---
+++ agendas/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class Meeting(models.Model):
+    date = models.DateTimeField()
+    name = models.CharField(max_length=200)
+    agenda_id = models.CharField(max_length=200)
+
+
+class AgendaItem(models.Model):
+    meeting = models.ForeignKey(Meeting)
+    section = models.TextField(blank=True)
+    number = models.IntegerField()
+    text = models.TextField()
+    minutes = models.TextField(blank=True)
+    backup = models.TextField(blank=True)
