539dceb02d2693fbd8104daa071a3fcf0aad111a
groovecoder <luke.crouch@gmail.com>
2014-02-23 19:04:41 -0600

    save alert sent

--- send_alerts.py
+++ send_alerts.py
@@ -17,9 +17,10 @@ def main():
             account_sid = "ACb395c103ff24402c9caf5504c7d10cdb"
             auth_token = "cc124f08b5d4ba53f413c94924b9cf16"
             client = TwilioRestClient(account_sid, auth_token)
- 
+
             message = client.messages.create(to=alert.phone, from_="9185508625", body="Agenda Item number: %d is next!" % alert.item.number)
             alert.sent = True
+            alert.save()
 
             print('alert sent')
 main()
