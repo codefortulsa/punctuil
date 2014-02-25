64f038f025cd243a0db4b5b54d0b08279ea83603
Ian Riley <ian.riley.915@gmail.com>
2014-02-24 22:41:47 -0600

    livestream added to scheduler

--- clock.py
+++ clock.py
@@ -10,6 +10,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "punctuil_django.settings")
 from apscheduler.scheduler import Scheduler
 import load_agendas
 import load_meeting_list
+import scrape_livestream
 import send_alerts
 
 sched = Scheduler()
@@ -17,7 +18,10 @@ sched = Scheduler()
 @sched.interval_schedule(minutes=1)
 def timed_job():
     #scrape the live feed every minute
-    send_alerts.main()
+    item_number = scrape_livestream.get_current_item()
+    # send out the appropriate alerts
+    if item_number != None:
+        send_alerts.main()
 
 @sched.cron_schedule(day_of_week='mon-sun', hour=0)
 def scheduled_job():
