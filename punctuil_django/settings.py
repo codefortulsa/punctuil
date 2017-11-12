76be6226f90b1987f68445066ff133b84d8b28b9
groovecoder <luke.crouch@gmail.com>
2017-11-12 07:54:33 -0600

    enable auth context processor for admin interface

--- punctuil_django/settings.py
+++ punctuil_django/settings.py
@@ -105,6 +105,11 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'APP_DIRS': True
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+            ]
+        }
     }
 ]
